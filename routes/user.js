e2f640a86762eb08e9a72b58cb94f9ae90fe3047
Mayank Sharma <mayankece155@gmail.com>
2021-04-16 11:15:53 +0530

    modified on routes

--- routes/user.js
+++ routes/user.js
@@ -2,4 +2,8 @@ const {User} = require('../models/user');
 const express = require('express');
 const router = express.Router();
 
+router.get(`/`, async (req, res) =>{
+    res.send("user-routes");
+})
+
 module.exports =router;
