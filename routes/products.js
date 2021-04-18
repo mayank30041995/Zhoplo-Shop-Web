2a26a530a98543bb073be40e4d11efabcb28f30b
Mayank Sharma <mayankece155@gmail.com>
2021-04-18 18:55:11 +0530

    added get, put, patch , delete routes of products

--- routes/products.js
+++ routes/products.js
@@ -5,8 +5,8 @@ const router = express.Router();
 const mongoose = require("mongoose");
 
 router.post("/", async (req, res) => {
-    const category =await Category.findById(req.body.category);
-    if(!category) return res.status(400).send('Invalid Category');
+  const category = await Category.findById(req.body.category);
+  if (!category) return res.status(400).send("Invalid Category");
   let product = new Product({
     name: req.body.name,
     description: req.body.description,
@@ -18,12 +18,69 @@ router.post("/", async (req, res) => {
     rating: req.body.rating,
     numReviews: req.body.numReviews,
     isFeatured: req.body.isFeatured,
-  })
-  product= await product.save();
+  });
+  product = await product.save();
 
-  if(!Product)
-  return res.status(500).send('The Product cannot be created')
+  if (!Product) return res.status(500).send("The Product cannot be created");
   res.send(product);
 });
 
-module.exports= router;
+router.get('/', async (req, res) => {
+    const productList = await Product.find();
+    if(!productList) {
+        res.status(500).status({success: false})
+    }
+    res.send(productList);
+})
+
+router.get('/:id', async (req, res) =>{
+    const product= await Product.findById(req.params.id);
+
+    if(!product) {
+        res.status(500).json({status: false})
+    }
+    res.send(product);
+})
+
+router.put('/:id', async (req, res) => {
+    const category = await Category.findById(req.body.category)
+    if(!category) return res.status(400).send('Invalid Category')
+
+    const product = await Product.findByIdAndUpdate(
+        req.params.id,
+        {
+            name: req.body.name,
+            description: req.body.description,
+            richDescription: req.body.richDescription,
+            image: req.body.image,
+            brand: req.body.brand,
+            price: req.body.price,
+            category: req.body.category,
+            countInStock: req.body.countInStock,
+            rating: req.body.rating,
+            numReviews: req.body.numReviews,
+            isFeatured: req.body.isFeatured,
+        },
+        { new: true}
+        )
+
+        if(!product)
+        return res.status(500).send('The product cannot be updated!!')
+
+        res.send(product)
+})
+
+router.delete('/:id', (req, res)=> {
+    Product.findByIdAndRemove(req.params.id).then(product => {
+        if(product) {
+            return res.status(200).json({success:true, message: 'The product is deleted!'})
+        } else {
+            return res.status(404).json({status:false,  message: "product not found!"})
+        }
+    }).catch(err=>{
+        return res.status(500).json({success: false, error:err})
+    })
+})
+
+
+module.exports = router;
