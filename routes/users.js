edbeed03afdd2d8c5a96217df2ba16787ddf4f46
Mayank Sharma <mayankece155@gmail.com>
2021-04-19 14:51:59 +0530

    get or get user by :id

--- routes/users.js
+++ routes/users.js
@@ -4,7 +4,21 @@ const router= express.Router();
 const bcrypt= require('bcryptjs');
 
 router.get(`/`, async (req, res) =>{ 
-    res.send("user-routes");
+    const userList= await User.find().select('-passwordHash');
+
+    if(!userList) {
+        res.status(500).json({success: false})
+    }
+    res.send(userList);
+})
+
+router.get('/:id', async(req,res) => { 
+    const user=await User.findById(req.params.id).select('-passwordHash')
+    
+    if(!user) {
+        res.status(500).json({message: 'The user with the given Id was not found'})
+    }
+    res.status(200).send(user);
 })
 
 router.post('/', async (req, res) => {
