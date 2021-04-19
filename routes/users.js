028a052f3498528fd283ddc6ea05f71df049d020
Mayank Sharma <mayankece155@gmail.com>
2021-04-19 17:55:47 +0530

    login api

--- routes/users.js
+++ routes/users.js
@@ -37,10 +37,56 @@ router.post('/', async (req, res) => {
     user = await user.save();
 
     if(!user)
+    return res.status(400).send('The user cannot be created!')
+
+    res.send(user);   
+})
+
+router.post('/login', async (req, res) => {
+    const user =await User.findOne({email: req.body.email})
+    const secret = process.env.secret;
+
+    if(!user) {
+         return res.status(400).send('The user not found');
+    }
+    user &&   res.status(200).send({user: user.email})
+     
+   
+  
+
+})
+
+router.post('/register', async (req, res) => {
+    let user = new User({
+        name: req.body.name,
+        email: req.body.email,
+        passwordHash: bcrypt.hashSync(req.body.password, 10),
+        phone: req.body.phone,
+        isAdmin: req.body.isAdmin,
+        street: req.body.street,
+        apartment: req.body.apartment,
+        zip: req.body.zip,
+        city: req.body.city,
+        country: req.body.country
+    })
+    user = await user.save();
+
+    if(!user)
     return res.status(400).send('the user cannot be created!')
 
     res.send(user);   
 })
 
+router.delete('/:id', (req, res) => {
+    User.findByIdAndRemove(req.params.id).then(user => {
+        if(user) {
+            return res.status(200).json({success: true, message: 'The user has been deleted!'})
+        }else {
+            return res.status(404).json({success: false, message: 'user not found for that id'})
+        }
+    }).catch(err =>{
+        return res.status(500).json({success: false, error: err})
+    })
+})
 
 module.exports =router;
