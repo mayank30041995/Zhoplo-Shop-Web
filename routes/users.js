8577a62150c0c80ebaf73bcdd1fc61c8d8b2b47b
Mayank Sharma <mayankece155@gmail.com>
2021-04-19 18:31:32 +0530

    getting access token after login

--- routes/users.js
+++ routes/users.js
@@ -2,6 +2,7 @@ const {User} = require('../models/user');
 const express = require('express');
 const router= express.Router();
 const bcrypt= require('bcryptjs');
+const jwt = require('jsonwebtoken');
 
 router.get(`/`, async (req, res) =>{ 
     const userList= await User.find().select('-passwordHash');
@@ -49,11 +50,18 @@ router.post('/login', async (req, res) => {
     if(!user) {
          return res.status(400).send('The user not found');
     }
-    user &&   res.status(200).send({user: user.email})
-     
-   
-  
-
+    if(user && bcrypt.compareSync(req.body.password, user.passwordHash)) {
+        const token = jwt.sign(
+            {
+                userId: user.id
+            },
+            secret,
+            {expiresIn : '1d'}
+        )
+        res.status(200).send({user: user.email, token: token})
+    } else {
+        res.status(400).send('password is wrong!');
+    }
 })
 
 router.post('/register', async (req, res) => {
